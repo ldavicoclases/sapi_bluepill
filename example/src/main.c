698c2bc017e0b23cd7bc40ce8289fa03a7a51012
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-22 10:46:28 -0300

    Clean main example

--- example/src/main.c
+++ example/src/main.c
@@ -20,8 +20,6 @@
 #include "main.h"
 #include "sapi.h"
 
-void SystemClock_Config(void);
-
 /**
   * @brief  The application entry point.
   * @retval int
@@ -30,18 +28,11 @@ int main(void)
 {
     uint8_t i;
     gpioMap_t secuencia[] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7,
-					         PA8, PA9, PA10, PA11, PA12, PA15,
-   	   	   	   	   	   	   	 PB0, PB1, PB3, PB4, PB5, PB6, PB7,
-   	   	   	   	   	   	     PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15};
+                             PA8, PA9, PA10, PA11, PA12, PA15,
+                             PB0, PB1, PB3, PB4, PB5, PB6, PB7,
+                             PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15};
 
-    HAL_Init();
-
-    SystemClock_Config();
-
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+    boardInit();
 
     for( i=0 ; i<sizeof(secuencia) ; i++ ) {
     	gpioInit(secuencia[i], GPIO_OUTPUT);
@@ -55,74 +46,5 @@ int main(void)
 		}
     }
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /**Configure the Systick interrupt time
-  */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-  /**Configure the Systick
-  */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-}
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-}
-#endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
