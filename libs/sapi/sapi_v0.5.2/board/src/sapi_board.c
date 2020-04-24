95b8666c249b3d7a4d4f7a2e198e73aee78afa32
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-23 23:16:51 -0300

    Revise clock configuration

--- libs/sapi/sapi_v0.5.2/board/src/sapi_board.c
+++ libs/sapi/sapi_v0.5.2/board/src/sapi_board.c
@@ -70,12 +70,32 @@ void boardInit(void)
    // Read clock settings and update SystemCoreClock variable
    SystemCoreClockUpdate();
 
+   /**Configure the Systick interrupt time
+   */
+   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+   /**Configure the Systick
+   */
+   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+   /* SysTick_IRQn interrupt configuration */
+   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
    // cyclesCounterInit( SystemCoreClock );
 
    // Inicializar el conteo de Ticks con resolucion de 1ms (si no se usa freeRTOS)
    #ifndef USE_FREERTOS
       tickInit( 1 );
    #endif
+
+   __HAL_RCC_AFIO_CLK_ENABLE();
+   __HAL_RCC_PWR_CLK_ENABLE();
+
+   /* System interrupt init*/
+
+   /** NOJTAG: JTAG-DP Disabled and SW-DP Enabled
+   */
+   __HAL_AFIO_REMAP_SWJ_NOJTAG();
 
    // GPIO Ports Clock Enable
    __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -86,38 +106,30 @@ void boardInit(void)
 /* System Clock Configuration */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+   /** Initializes the CPU, AHB and APB busses clocks
+   */
+   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+   RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+   RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+   RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+   RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+   RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL6;
+   HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+   /** Initializes the CPU, AHB and APB busses clocks
+   */
+   RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                    |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+   RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
-
-    /**Configure the Systick interrupt time
-    */
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-    /**Configure the Systick
-    */
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-    /* SysTick_IRQn interrupt configuration */
-    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
 }
 
 /*==================[end of file]============================================*/
