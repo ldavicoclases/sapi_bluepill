dbf8a2c0ac9cefe71b370645989646f8ae057c23
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-05-03 20:15:05 -0300

    Add USB device files

--- libs/STM32F1xx_HAL/inc/stm32f1xx_hal_conf.h
+++ libs/STM32F1xx_HAL/inc/stm32f1xx_hal_conf.h
@@ -78,6 +78,7 @@
 #define HAL_UART_MODULE_ENABLED
 #define HAL_ADC_MODULE_ENABLED
 #define HAL_RTC_MODULE_ENABLED
+#define HAL_PCD_MODULE_ENABLED
 
 /* ########################## Oscillator Values adaptation ####################*/
 /**
